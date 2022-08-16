129f70bf8a808c30e3d173e63d21cf396bc64f42
Hans Christian Zoll <christianzoll@yahoo.com>
2022-08-16 11:58:58 -0600

    Better variable and class names

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Calculator() {
   document.addEventListener('DOMContentLoaded', () => {
-    let digitKeys = 10;
-    for (let i=0; i < digitKeys; i++) {
+    let digits = 10;
+    for (let i=0; i < digits; i++) {
       
       const textNode = document.createTextNode(i); // last/deepest child
 
@@ -15,34 +15,31 @@ function Calculator() {
       textElement.setAttribute('y', 67.25);
       textElement.setAttribute('font-size', 50);
       textElement.appendChild(textNode);  // append deepest child to first parent
-      
+      console.log(textElement)
       const circle = document.createElementNS('http://www.w3.org/2000/svg','circle') //svg elements require createElementNS() (NS = namespace) instead of createElement();
-      circle.setAttribute('class', 'digitKeys');
+      circle.setAttribute('class', 'digitCircle');
       circle.setAttribute('cx', 50)
       circle.setAttribute('cy', 50)
       circle.setAttribute('r', 33)
       circle.setAttribute('fill', 'steel') // sibling of first parent
-      console.log(circle)
+console.log(circle)
       
-      const circlesSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')//svg elements require createElementNS() (NS = namespace) instead of createElement();
-      circlesSVG.setAttribute('id', 'circlesSVG')
-      circlesSVG.setAttribute('width', 100)
-      circlesSVG.setAttribute('height', 100)
-      circlesSVG.appendChild(circle);
-      circlesSVG.appendChild(textElement);
-      console.log(circlesSVG)
-      
-      
-      const keyboardDiv = document.createElement('div')
-      keyboardDiv.setAttribute('class', 'keyboard')
-      keyboardDiv.appendChild(circlesSVG)
-        console.log(keyboardDiv)
-    document.body.appendChild(keyboardDiv);
-    const appsJsReturnDiv = document.querySelector('.App')
-      appsJsReturnDiv.appendChild(keyboardDiv)
-        console.log(appsJsReturnDiv)
-  }
-}
-)}
+      const digitSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')//svg elements require createElementNS() (NS = namespace) instead of createElement();
+      digitSVG.setAttribute('id', 'digitSVG')
+      digitSVG.setAttribute('width', 100)
+      digitSVG.setAttribute('height', 100)
+      digitSVG.appendChild(circle);
+      digitSVG.appendChild(textElement);
+console.log(digitSVG)
+      const digitKey = document.createElement('div')
+      digitKey.setAttribute('class', 'digitKey')
+      digitKey.appendChild(digitSVG)
+console.log(digitKey)
+      const App = document.querySelector('.App')
+      App.appendChild(digitKey)
+      console.log(App)
+    } // end of for loop
+  } // end of .addEventListener function parameter
+  )} // end of .addEventListener function definition
 
 export default Calculator
