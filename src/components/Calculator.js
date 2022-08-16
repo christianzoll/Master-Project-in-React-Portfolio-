752ac33fba9b9ffb2ce943afb1b2cdea0e34b5cd
Hans Christian Zoll <christianzoll@yahoo.com>
2022-08-16 12:19:59 -0600

    changed const to var

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,7 +7,7 @@ function Calculator() {
       
       const textNode = document.createTextNode(i); // last/deepest child
 
-      const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text'); // first/deepest parent
+      var textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text'); // first/deepest parent
       textElement.setAttribute('fill', 'white');
       textElement.setAttribute('alignment-baseline', "middle");
       textElement.setAttribute('text-anchor', "middle");
@@ -15,31 +15,38 @@ function Calculator() {
       textElement.setAttribute('y', 67.25);
       textElement.setAttribute('font-size', 50);
       textElement.appendChild(textNode);  // append deepest child to first parent
-      console.log(textElement)
-      const circle = document.createElementNS('http://www.w3.org/2000/svg','circle') //svg elements require createElementNS() (NS = namespace) instead of createElement();
+      
+      var circle = document.createElementNS('http://www.w3.org/2000/svg','circle') //svg elements require createElementNS() (NS = namespace) instead of createElement();
       circle.setAttribute('class', 'digitCircle');
       circle.setAttribute('cx', 50)
       circle.setAttribute('cy', 50)
       circle.setAttribute('r', 33)
       circle.setAttribute('fill', 'steel') // sibling of first parent
-console.log(circle)
+
       
-      const digitSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')//svg elements require createElementNS() (NS = namespace) instead of createElement();
+      var digitSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')//svg elements require createElementNS() (NS = namespace) instead of createElement();
       digitSVG.setAttribute('id', 'digitSVG')
       digitSVG.setAttribute('width', 100)
       digitSVG.setAttribute('height', 100)
       digitSVG.appendChild(circle);
       digitSVG.appendChild(textElement);
-console.log(digitSVG)
-      const digitKey = document.createElement('div')
+
+      var digitKey = document.createElement('div')
       digitKey.setAttribute('class', 'digitKey')
       digitKey.appendChild(digitSVG)
-console.log(digitKey)
-      const App = document.querySelector('.App')
+
+      var App = document.querySelector('.App')
       App.appendChild(digitKey)
-      console.log(App)
+      
     } // end of for loop
+    console.log(typeof textElement)
+    console.log(typeof circle)
+    console.log(typeof digitSVG)
+    console.log(typeof digitKey)
+    console.log(typeof App)
   } // end of .addEventListener function parameter
+
   )} // end of .addEventListener function definition
+  
 
 export default Calculator
