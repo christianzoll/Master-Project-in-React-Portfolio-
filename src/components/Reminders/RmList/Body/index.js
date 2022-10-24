efd29f60a70d469b2efae6ecf72e23efbd4a56ef
Hans Christian Zoll <christianzoll@yahoo.com>
2022-10-24 05:57:53 -0600

    added body component

---
+++ src/components/Reminders/RmList/Body/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Body() {
+    return(
+        <div class="rm-list-body"></div>
+    );
+}
+
+export default Body;
--- src/components/Reminders/RmList/index.js
+++ src/components/Reminders/RmList/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Body from "./Body";
 
 function RmList() {
   return(
     <div className="rm-list">
     <Header></Header>
+    <Body></Body>
     <Footer></Footer>
     </div>
   );
