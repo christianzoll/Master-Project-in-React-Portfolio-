c61adf6698ccad85172b719aa98d78a8a0dd25b9
Hans Christian Zoll <christianzoll@yahoo.com>
2022-10-26 21:49:28 -0700

    removed extra spaces

--- src/components/Reminders/RmList/Footer/index.js
+++ src/components/Reminders/RmList/Footer/index.js
@@ -1,45 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
 function Footer(){
-
-  const [reminder, setReminder] = useState("");
-  
+  const [reminder, setReminder] = useState("");  
   const [reminders, setReminders] = useState([]);
-
-const handleSubmit = (e) => {
-     e.preventDefault();
-     if (reminder){
-       let newReminder = {
-         reminder: reminder,
-         complete: false,
-        };
-        setReminders([newReminder, ...reminders])
-        setReminder('');
-   } else {
-     setReminder("oops there's a problem");
-   }
-};
-
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if (reminder){
+        let newReminder = {
+          reminder: reminder,
+          complete: false,
+         };
+         setReminders([newReminder, ...reminders])
+         setReminder('');
+     } else {
+      setReminder("oops there's a problem");
+    }
+  };
   useEffect(() => {
     localStorage.setItem('reminders', JSON.stringify(reminders), [reminders])
   })
-
-    return(
-      <div className="rm-list-footer">
-        <form onSubmit={handleSubmit}>
-
-          <input 
-            id='newReminder' 
-            type='text'
-            value={reminder}
-            onChange={(e) => setReminder(e.target.value)}
-          />
-
-          <button type='submit'>+</button>
-
-        </form>
-      </div>
-    )
+  return(
+    <div className="rm-list-footer">
+      <form onSubmit={handleSubmit}>
+        <input 
+          id='newReminder' 
+          type='text'
+          value={reminder}
+          onChange={(e) => setReminder(e.target.value)}
+        />
+        <button type='submit'>+</button>
+      </form>
+    </div>
+  )
 }
-
 export default Footer;
