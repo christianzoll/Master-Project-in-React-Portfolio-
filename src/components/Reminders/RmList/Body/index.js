4c833bc7f7cbad63ed5be7cd139bd362e16f2e9d
Hans Christian Zoll <christianzoll@yahoo.com>
2022-11-02 12:49:05 -0600

    component cleanup

--- src/components/Reminders/RmList/Body/index.js
+++ src/components/Reminders/RmList/Body/index.js
@@ -3,7 +3,15 @@ import React from "react";
 function Body() {
     return(
         <div className="rm-list-body">
-            
+            <form>
+              {//onfocus 
+                //radio exists
+                //input text field
+              //offocus
+                //radio disappears
+                //input text field turns into paragraph
+            }
+            </form>
         </div>
     );
 }
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,17 +1,11 @@
 import React from "react"
 
-// function Timeline() {
-class Timeline extends React.Component{
-  render() {
-    //   React.createElement('div',  {className: 'timeline'}, 'Hello World');
+function Timeline() {
     return (
       <div className="timeline">
 
       </div>
-      //   console.log('Timeline shows up with the DOMContentLoaded document event listener')
       );
-  }
-  // }
 }
 
 export default Timeline
