bce073cc7509e3e6e1cd53cf32e29289ba0ac296
Hans Christian Zoll <christianzoll@yahoo.com>
2022-11-04 10:15:20 -0600

    fixed useEffect function

--- src/components/Reminders/RmList/index.js
+++ src/components/Reminders/RmList/index.js
@@ -4,14 +4,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Body from "./Body";
 
-function RmList() {
-  const [reminder, setReminder] = useState("");  
+function RmList() {  
   const [reminders, setReminders] = useState([]);
   return(
     <div className="rm-list">
     <Header></Header>
     <Body>{reminders}</Body>
-    <Footer setReminder={setReminder} setReminders={setReminders}></Footer>
+    <Footer  setReminders={setReminders}></Footer>
     </div>
   );
 }
