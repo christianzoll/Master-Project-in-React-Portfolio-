f0e3b3db61addcb1f6cc5c34bbe7fbc7f5c4d09c
Hans Christian Zoll <christianzoll@yahoo.com>
2022-12-26 11:22:23 -0700

    extracted callback from fetchReminders useEffect Hook

--- src/services/fetchReminders.js
+++ src/services/fetchReminders.js
@@ -1,12 +1,14 @@
 import { useEffect } from "react"
 
 function fetchReminders(){
+
+  const getEm = async () => {
+    const res = await fetch('https://official-joke-api.appspot.com/random_joke'); //replace with mongodb api
+    const resData = await res.json();
+    console.log(resData);
+  } 
+
   useEffect(() => {
-    const getEm = async () => {
-      const res = await fetch('https://official-joke-api.appspot.com/random_joke'); //replace with mongodb api
-      const resData = await res.json();
-      console.log(resData);
-    } 
     getEm();
   }, [])
 }  
